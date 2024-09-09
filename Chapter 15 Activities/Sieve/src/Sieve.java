ea44ceabfd5b26dd3d52907eb2610ffe073ac20d
GrumpyMusician <theverygrumpymusician@gmail.com>
2024-09-09 18:59:34 -0500

    Update Sieve.java

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 
 /**
@@ -9,17 +11,26 @@ public class Sieve
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.println("Compute primes up to which integer?");
+        System.out.print("Compute primes up to which integer? ");
         int n = in.nextInt();
 
         // Your work goes here
-        . . .
+        Set<Integer> allnums = new HashSet<>();
 
+        for (int i = 1; i <= n; i++){
+            allnums.add(i);
+        }
 
+        for (int i = 2; i <= n; i++){
+            for (int j = 2; j <= n; j++){
+                allnums.remove(i*j); // I'm suprised this part worked first try :p
+            }
+                
+        }
 
+        allnums.remove(1); // 1 can not be a prime number, since prime numbers require exactly two factors.
 
-
-
+        System.out.println("Your prime numbers are: " + allnums);
 
     }
 }
