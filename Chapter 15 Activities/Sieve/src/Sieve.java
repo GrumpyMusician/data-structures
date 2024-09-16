0058133f15741b6e0e79af34140bdd4264595b15
GrumpyMusician <theverygrumpymusician@gmail.com>
2024-09-16 07:33:49 -0500

    SHOVE IT (into github)

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -17,7 +17,7 @@ public class Sieve
         // Your work goes here
         Set<Integer> allnums = new HashSet<>();
 
-        for (int i = 1; i <= n; i++){
+        for (int i = 2; i <= n; i++){
             allnums.add(i);
         }
 
@@ -27,8 +27,6 @@ public class Sieve
             }
                 
         }
-
-        allnums.remove(1); // 1 can not be a prime number, since prime numbers require exactly two factors.
 
         System.out.println("Your prime numbers are: " + allnums);
 
